bf48588584624ca5867252b42000074f4672c4d5
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2023-08-05 17:47:42 +0100

    render update

--- src/service/authService.js
+++ src/service/authService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { toast } from "react-toastify";
 
 // const BACKEND_URL = process.env.REACT_BACKEND_URL;
-const BACKEND_URL = 'http://localhost:5000'
+const BACKEND_URL = 'https://parachbackend.onrender.com/'
 
 export const validateEmail = (email) => {
     return email.match( /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
