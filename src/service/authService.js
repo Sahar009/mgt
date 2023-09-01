f5ef8faf4a255cbcf0ea3e57b21162f9825b186d
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2023-09-01 17:36:54 +0100

    Update authService.js

--- src/service/authService.js
+++ src/service/authService.js
@@ -13,7 +13,7 @@ export const validateEmail = (email) => {
         try {
             const response = await axios.post(`http://localhost:5000/api/users/register`,userData, {withCredentials: true})
             if (response.statusText === "OK"){
-                toast.success('Registered successfully')
+                toast.success('Registered successfullyvvvvv')
             }
             return response.data
         } catch (error) {
